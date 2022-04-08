9881ec9253c5cb33a4343524d7b8ae4241a27a2e
niallh <n.hargreaves@prospects.ac.uk>
2022-04-08 12:26:25 +0100

    added button to call API to retrieve authToken

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+    type AuthToken = () => {
+        tokenType: string,
+        expiresIn: number,
+        extExpiresIn: number,
+        expiresOn: number,
+        notBefore: number,
+        resource: string,
+        accessToken: string
+    }
+
+    const axios = require('axios');
+
+    //const authToken: AuthToken [authToken, setAuthToken] = useState: any(null);
+
+    const getAuthToken = () => {
+        axios.post("https://login.microsoftonline.com/48f9394d-8a14-4d27-82a6-f35f12361205/oauth2/token", {
+            proxy: {
+                host: 'localhost',
+                port: 3000
+            }
+        })
+            .then(function (response: any){
+            console.log(response)
+            //setAuthToken(response)
+        })
+        //console.log(authToken);
+    }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +48,14 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+        <h1>Help!</h1>
+        <ul>
+            <li> </li>
+        </ul>
+      <button onClick={() => getAuthToken()}/>
+
+      </body>
     </div>
   );
 }
